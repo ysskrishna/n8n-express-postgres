df3323e384f834d2f520185b9e250e7bdc8a8750
ysskrishna <sivasaikrishnassk@gmail.com>
2025-07-12 15:27:45 +0530

    Siva | minor issue with parsing int

--- express/db.js
+++ express/db.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: parseInt(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
